ab6d52e66a83d2b8b8e2c997a14867ee4af76070
Shaw Clifton <shawwc@uw.edu>
2023-10-29 12:43:24 -0700

    big motion

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ let icon = document.getElementsByClassName("icon")[0];
 // Add an event listener for the 'click' event on the icon element
 icon.addEventListener('click', responsive_control);
 
+</script>
 // Function to control the responsiveness of the navigation bar
 function responsive_control() {
   // Get the element with the id "myTopnav"
